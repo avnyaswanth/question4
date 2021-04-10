ca8444da75d22a178a4162ce5ab35cfd010da08f
avnyaswanth <yaswanthalapati20@gmail.com>
2021-04-10 12:20:18 +0530

    ques4

---
+++ loginform/forms.py
@@ -0,0 +1,5 @@
+# from django import forms
+
+# class Loginform(forms.Form):
+#     email = forms.EmailField(max_length=100)
+#     password = forms.CharField(max_length=32, widget=forms.PasswordInput)
